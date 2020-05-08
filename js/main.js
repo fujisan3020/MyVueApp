431f1a59094a1b3742e279567edb03aa82210d7f
fujisan <gashigashi3040@gmail.com>
2020-05-08 17:29:19 +0900

    v-onでイベントを設定しよう

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@
   'use strict';
 
   // two way data binding (to UI)
+  // data と UI を結び付けるという意味で、双方向というのは data を更新すれば UI が更新されて、 UI を更新すれば data が更新されるという意味
 
   var vm = new Vue({
     // el: Element
     el: '#app',
     data: {
-      name: 'fujisan',
+      newItem: '',
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3',
+      ]
+    },
+    methods: {
+      // addItem: function(e) {
+      //   e.preventDefault();
+      //   this.todos.push(this.newItem);
+      // }
+      addItem: function() {
+        //newItemの内容がtodosの末尾に追加される
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
     }
   });
 })();
